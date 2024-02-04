a25ff2fbc9b7e5bb46967bbde70e21fd21f989d3
jakebroffman <jake.broffman1088@gmail.com>
2024-02-04 18:27:25 -0500

    finish validations, add controller actions to user controller

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,13 @@ class Role < ApplicationRecord
   belongs_to :user
   belongs_to :event
   has_many :rsvps
+
+  validates :title, presence: true
+  validates :responsibilities, presence: true
+  validates :paid, inclusion: { in: [true, false], message: "must be true or false" }
+  validates :user, presence: true
+  validates :event, presence: true
+  validates :title, uniqueness: { scope: :event_id, message: "title must be unique within an event" }
+  validates :user_id, uniqueness: { scope: :event_id, message: "user can have only one role per event" }
 end
+
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,4 +2,14 @@ class Rsvp < ApplicationRecord
   belongs_to :user
   belongs_to :event
   belongs_to :role
+
+
+  validates :user, presence: true
+  validates :event, presence: true
+  validates :role, presence: true
+  validates :comment, length: { maximum: 255 } 
+  validates :attending, inclusion: { in: [true, false], message: "must be true or false" }
+  validates :user_id, uniqueness: { scope: :event_id, message: "already RSVP'd to this event" }
+end
+
 end
