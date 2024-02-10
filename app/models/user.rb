6d2aa5ab831f63998a8b0b986dad243afe52d13c
jakebroffman <jake.broffman1088@gmail.com>
2024-02-10 16:45:15 -0500

    backup commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
     has_many :events, through: :roles
     has_many :rsvps
 
-    validates :username, presence: true, uniqueness: { scope: :id, message: "Username is already taken" }, on: :create
-    validates :email, presence: true, uniqueness: { scope: :id, message: "Email is already taken" }, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :create
+    validates :username, presence: true, uniqueness: { case_sensitive: false, scope: :id, message: "Username is already taken" }, on: :create
+    validates :email, presence: true, uniqueness: { case_sensitive: false, scope: :id, message: "Email is already taken" }, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :create
+
     validates :password, presence: true, length: { minimum: 6 }, on: :create
     validates :city, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "can only contain letters and spaces" }
     validates :state, presence: true, length: { is: 2 }, format: { with: /\A[A-Z]+\z/, message: "must be two uppercase letters" }
